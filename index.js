64c525e848b4729be10b1454a486e9ab11523e16
nif33 <jennifer_dai2000@yahoo.ca>
2019-10-14 16:19:12 -0700

    added npm

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/users', (req,res) => {
+    var getTokimonQuery = `SELECT * FROM tokimon`;
+    console.log(getTokimonQuery);
+    pool.query(getTokimonQuery, (error, result) => {
+      if (error)
+        res.end(error);
+      var results = {'rows': result.rows };
+      console.log(results);
+      res.render('pages/tokimon', results)
+    })
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
