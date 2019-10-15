ecda254fd974a22b945d9400aac8cf28c6218370
nif33 <jennifer_dai2000@yahoo.ca>
2019-10-14 17:52:03 -0700

    initial push to github

--- index.js
+++ index.js
@@ -3,28 +3,13 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
-/*var pool;
-if(process.env.DATABASE_URL){
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-  });
-}
-else{
-  pool = new Pool({
-    user: 'postgres'
-    host: 'localhost'
-    database: 'tokimon'
-    password: 'password'
-  });
-}*/
 const pool = new Pool({
   user: 'postgres'
   host: 'localhost'
   database: 'tokimon'
   password: 'password'
+  connectionString: process.env.DATABASE_URL
 })
-//  connectionString: process.env.DATABASE_URL
-//});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -37,15 +22,14 @@ express()
       if (error)
         res.end(error);
       var results = {'rows': result.rows };
-      //obj = {print: result};
       res.render('pages/index', results)
     })
   })
-  .get('/db', async(req,res)=>{
+  /*.get('/db', async(req,res)=>{
     const client = await pool.connect()
     const result = await client.query('SELECT * FROM tokimon')
     const results = {'rows': result.rows };
     res.render('pages/db', results)
     client.release();
-  })
+  })*/
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
