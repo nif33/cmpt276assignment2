6ccc3baea5cde84a363294db5c0ecd66135786ac
nif33 <jennifer_dai2000@yahoo.ca>
2019-10-15 22:00:02 -0700

    added deletion feature

--- index.js
+++ index.js
@@ -4,32 +4,36 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres'
-  host: 'localhost'
-  database: 'tokimon'
-  password: 'password'
   connectionString: process.env.DATABASE_URL
 })
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/index', (req,res) => {
+  .get('/tokidex', (req,res) => {
     var getTokimonQuery = `SELECT * FROM tokimon`;
     pool.query(getTokimonQuery, (error, result) => {
       if (error)
         res.end(error);
       var results = {'rows': result.rows };
-      res.render('pages/index', results)
+      res.render('pages/tokidex', results)
     })
   })
-  /*.get('/db', async(req,res)=>{
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM tokimon')
-    const results = {'rows': result.rows };
-    res.render('pages/db', results)
-    client.release();
-  })*/
+  .get('/delete', (req, res) => res.render('pages/delete'))
+  .post('/delete', async (req, res) => {
+  const client = await pool.connect();
+  await client.query('DELETE FROM tokimon WHERE name = $1', [req.body.name], function(err, result) {
+    if (err) {
+      console.error(err);
+      res.status(400).send(err);
+    }
+  });
+  client.release();
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
